a1405fc0a15d9f4fddda17085186bda548867b7b
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2025-06-29 10:58:30 -0300

    split mainPy.py

--- src/webWorker.ts
+++ src/webWorker.ts
@@ -1,4 +1,5 @@
 import { loadPyodide } from "pyodide";
+import mainPy from './mainPy.py';
 
 async function mainPyodide(windowLocationIndex: string) {
     console.log("mainPyodide", location, windowLocationIndex);
@@ -13,47 +14,7 @@ async function mainPyodide(windowLocationIndex: string) {
     let pyodide = await loadPyodide({ indexURL, packages });
     console.log("loaded pyodide");
 
-    const pyresult = pyodide.runPython(`
-import js
-from pyodide.ffi import to_js
-
-from anita.anita_pt_fo import check_proof, ParserAnita
- 
-
-def my_check_proof(inp):
-    if inp == "start":
-        return to_js("start")
-    textResponse = check_proof(inp, None, True, True, False)
-    try:
-        getProofResponse = ParserAnita.getProof(inp)
-        print(6, getProofResponse)
-        getProofResponse = {
-            'latex': getProofResponse.latex,
-#           'errors': getProofResponse.errors,
-#           'premisses': getProofResponse.premisses,
-#           'conclusion': getProofResponse.conclusion,
-#           'is_closed': getProofResponse.is_closed,
-#           'theorem':getProofResponse.theorem,
-#           'latex_theorem': getProofResponse.latex_theorem,
-            'colored_latex': getProofResponse.colored_latex,
-#           'counter_examples': getProofResponse.counter_examples,
-        }
-
-        print(5, getProofResponse)
-        print(4, repr(getProofResponse))
-        converted = to_js(getProofResponse, create_pyproxies=False, dict_converter=js.Object.fromEntries)
-        print(3, converted)
-    except e:
-        print(2, e)
-        getProofResponse = None
-    ret = (textResponse, converted)    
-    print("1", ret)
-    ret = to_js(ret, create_pyproxies=False, dict_converter=js.Object.fromEntries)
-    print(7, ret)
-    return ret
-
-"ok"
-`);
+    const pyresult = pyodide.runPython(mainPy);
     console.log("ran pyodide");
     console.log(pyresult);
     console.log(typeof pyresult);
