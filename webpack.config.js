bce1388e7c07bf12933220a6c39adce54ca5853a
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2025-07-19 11:20:46 -0300

    writeToDisk

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
     devServer: {
         static: './dist',
         hot: false,
+        devMiddleware: {
+            writeToDisk: true,
+        },
     },
     module: {
         rules: [
